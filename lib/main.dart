4f2e3c7e1a951acdbe9772931b287215007632b1
Emilio kariuki <emilio113kariuki@gmail.com>
2022-07-13 18:04:20 +0300

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,115 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Home(),
+    home: Complete(),
   ));
 }
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class Complete extends StatefulWidget {
+  const Complete({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  State<Complete> createState() => _CompleteState();
 }
 
-class _HomeState extends State<Home> {
+class _CompleteState extends State<Complete> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Center(
-                child: CircleAvatar(
-                  radius: 80,
-                  backgroundColor: Colors.red,
-                ),
-              ),
-              const Divider(
-                color: Colors.black,
-                thickness: 2,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Name:',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text("EMilio Kariuki",
-                      style: TextStyle(
-                        fontSize: 25,
-                      ))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Age:',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text("19",
-                      style: TextStyle(
-                        fontSize: 25,
-                      ))
-                ],
-              ),
-              const Divider(
-                color: Colors.black,
-                thickness: 2,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'School:',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text("Flutter",
-                      style: TextStyle(
-                        fontSize: 25,
-                      ))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Id:',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text("10",
-                      style: TextStyle(
-                        fontSize: 25,
-                      ))
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Icon(
-                    Icons.email,
-                    size: 35,
-                  ),
-                  Icon(Icons.person_add, size: 35),
-                  Icon(Icons.notification_add, size: 35)
-                ],
-              )
-            ]),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "FLUTTER ID",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
       ),
-    ));
+    );
+  }
+
+  Widget showText(String text) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+    );
   }
 }
