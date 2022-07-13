e981ef8af4dbcbb4fe2509c176be19c33593f99d
Emilio kariuki <emilio113kariuki@gmail.com>
2022-07-13 18:43:18 +0300

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -19,18 +19,56 @@ class _CompleteState extends State<Complete> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text(
-          "FLUTTER ID",
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-        ),
+        title: showText("FLUTTER ID", Colors.white),
+      ),
+      body: Column(
+        children: [cardInformation()],
       ),
     );
   }
 
-  Widget showText(String text) {
+  Widget showText(String text, Color choice) {
     return Text(
       text,
-      style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      style:
+          TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: choice),
+    );
+  }
+
+  Widget cardInformation() {
+    return Row(
+      children: [
+        const CircleAvatar(
+          backgroundColor: Colors.red,
+          radius: 30,
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            showText("Emilio Kariuki", Colors.black),
+            showText("emilio113kariuki@gmail.com", Colors.green)
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget rowText(String text_1, String text_2) {
+    return Row(
+      children: [
+        showText(text_1, Colors.blue),
+        const SizedBox(
+          width: 7,
+        ),
+        showText(":", Colors.red),
+        const SizedBox(
+          width: 7,
+        ),
+        showText(text_2, Colors.black)
+      ],
     );
   }
 }
